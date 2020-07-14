24a45fcc46c1881537822d6fd74a7e9312adef0a
Christian Reyes <creyes.prog@gmail.com>
2020-07-14 16:03:55 -0400

    [POR-8] Added skill bar columns and updated some CSS/assets
    1. Logos updated
    2. Bars added to skill section
    3. Updated some content (still need to add more)

---
+++ portfolio/portfolio/Infrastructure/Models/PartialModels/SkillPartialViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portfolio.Infrastructure.Models.PartialModels
+{
+    public class SkillPartialViewModel
+    {
+        public SkillPartialViewModel(string header, int weight)
+        {
+            Header = header;
+            Weight = weight;
+        }
+
+        public string Header { get; set; }
+        public int Weight { get; set; }
+    }
+}
