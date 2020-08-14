95ffbebf009bfd4b0fd95f36d8ea37ddc1bb5756
Christian Reyes <creyes.prog@gmail.com>
2020-08-13 23:16:53 -0400

    Add keyframe animations as well as on scroll triggers

--- portfolio/portfolio/wwwroot/js/animation.js
+++ portfolio/portfolio/wwwroot/js/animation.js
@@ -4,6 +4,25 @@
 
     function init() {
         animateNextLine();
+        animateContactOnView();
+        loadAnimations();
+        animateContactOnView();
+    }
+
+    // Find's which line to type out next. Callback / init func.
+    function animateNextLine(linePos = 0) {
+        var lines = [
+            ["> Hello world.", ".cmd-line1"],
+            ["> I'm Christian Reyes.", ".cmd-line2"],
+            ["> Full-stack web developer", ".cmd-line3"]
+        ];
+
+        if (linePos < lines.length) {
+            animateCmd(lines[linePos][0], $(lines[linePos][1]), animateNextLine, 0, linePos);
+        } else {
+            linePos--;
+            $(lines[linePos][1]).addClass("blink");
+        }
     }
 
     // Animate lines being typed
@@ -31,19 +50,31 @@
         }, 65);
     }
 
-    // Find's which line to type out next. Callback / init func.
-    function animateNextLine(linePos = 0) {
-        var lines = [
-            ["> Hello world.", ".cmd-line1"],
-            ["> I'm Christian Reyes.", ".cmd-line2"],
-            ["> Full-stack web developer", ".cmd-line3"]
-        ];
+    // When scrolling, determine what animations to trigger
+    var $animations;
 
-        if (linePos < lines.length) {
-            animateCmd(lines[linePos][0], $(lines[linePos][1]), animateNextLine, 0, linePos);
-        } else {
-            linePos--;
-            $(lines[linePos][1]).addClass("blink");
+    $(window).scroll(animateContactOnView);
+
+    function loadAnimations() {
+        $animations = [];
+
+        $("[animation]").each(function (i, e) {
+            $animations.push($(e));
+        });
+    }
+
+    function animateContactOnView() {
+        // Check if we still have any animations to go through
+        if ($animations !== undefined && $animations.length !== 0) {
+            // Loop through animations and trigger when at certain point
+            for (var i = 0; i < $animations.length; i++) {
+                if ($animations[i].position().top <= $("html").scrollTop() + 500) {
+                    $animations[i].removeClass("hide");
+                    $animations[i].addClass($animations[i].attr("animation"));
+                    $animations.splice(i, 1);
+                }
+            }
+
         }
     }
 });
